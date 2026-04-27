3f3b0be8a86eda5146013b9d97f7fed4aae08c5a
Eddie <EdwardTJM@gmail.com>
2026-04-27 10:42:56 +0800

    Added the attributes for weather types

---
+++ app.js
@@ -0,0 +1,13 @@
+const weatherLookup = {
+    0: { desc: "Clear Sky", icon: "☀️" },
+    1: { desc: "Mainly Clear", icon: "🌤️" },
+    2: { desc: "Partly Cloudy", icon: "⛅" },
+    3: { desc: "Overcast", icon: "☁️" },
+    45: { desc: "Fog", icon: "🌫️" },
+    48: { desc: "Rime Fog", icon: "🌫️" },
+    51: { desc: "Light Drizzle", icon: "🌦️" },
+    61: { desc: "Slight Rain", icon: "🌧️" },
+    63: { desc: "Moderate Rain", icon: "🌧️" },
+    80: { desc: "Rain Showers", icon: "🌦️" },
+    95: { desc: "Thunderstorm", icon: "⛈️" }
+};
